aed163d6e3c5e8b290c1d8e54ef5472dac8f6fbb
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-24 12:27:13 +0200

    component exports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,23 @@ import ThemedImage from "@/components/themed-image"
 import {defaultEditorContent, DefaultFormState} from "./defaults"
 import FAlert from "@/components/alert"
 import { ThemeProvider } from "./components/theme-provider"
+import { Alert, AlertTitle, AlertDescription } from "./components/ui/alert"
+import {Badge, badgeVariants } from "./components/ui/badge"
+import { buttonVariants } from "./components/ui/button"
+import { Button } from "./components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card"
+import { Checkbox } from "./components/ui/checkbox"
 
+export { Alert, AlertTitle, AlertDescription }
+export { Avatar, AvatarImage, AvatarFallback }
+export { Badge, badgeVariants }
+export {Button, buttonVariants}
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export { Checkbox }
+export {Combobox} from "./components/ui/combobox";
+export * from "./components/ui/dropdown-menu";
+export * from "./components/ui/hover-card"
 export {
   ThemedImage,
   ThemeProvider,
