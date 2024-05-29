49607ced22f35376d4608db03a5e14b1abcab43c
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-29 20:27:58 +0200

    changed ../../ to @/toolkit/

--- components/ui/3d-card.tsx
+++ components/ui/3d-card.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {cn} from "../../lib/utils";
+import {cn} from "@/toolkit/lib/utils";
 import React, {
   createContext,
   useState,
--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,4 +1,4 @@
-import { cn } from "../../lib/utils"
+import { cn } from "@/toolkit/lib/utils"
 import React from "react";
 
 function Skeleton({
